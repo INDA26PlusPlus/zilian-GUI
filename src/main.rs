4d04df62e5ed4fc6b78e720f313abf3c2dad3fea
zlinux <ziltriz@gmail.com>
2026-09-20 19:46:33 +0200

    Added GGEZ support. Fixes #3

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,82 @@
-// Exampel start of a program.
-use chess::*;
+//! Basic hello world example.
 
-// Init a full board.
-fn main() {
+use ggez::{event, graphics, Context, GameResult};
+use std::{env, path};
 
-    // init a board object.
-    let mut gameboard = Board::init_board();
-
-    // fills the created board object.
-    gameboard.fill_board();
-    
-    // Displays board with ascii in CLI
-    cli_display_board(&gameboard);
+// First we make a structure to contain the game's state
+struct MainState {
+    frames: usize,
 }
 
-// Method to display board in CLI
-fn cli_display_board (game: &Board) {
+impl MainState {
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        ctx.gfx.add_font(
+            "LiberationMono",
+            graphics::FontData::from_path(ctx, "/LiberationMono-Regular.ttf")?,
+        );
 
-    let mut rank: i32 = 0;
-    for square_number in (0..64) {
-
-        let current_square_piece = game.check_square(square_number);
-
-        // If this is the first square in the rank
-        // Print current rank and a | 
-        if square_number%8 == 0 {
-            rank += 1;
-
-            print!("{} | ", rank)
-        }
-
-        if square_number%8 == 7 {
-            println!("{} ", pice_to_char(current_square_piece) ) 
-        }
-        else {
-            print!("{} ", pice_to_char(current_square_piece) ) 
-        }
+        let s = MainState { frames: 0 };
+        Ok(s)
     }
-    println!("------------------");
-    println!("  | a b c d e f g h");
 }
 
-fn pice_to_char (piece: Piece) -> char {
+// Then we implement the `ggez:event::EventHandler` trait on it, which
+// requires callbacks for updating and drawing the game state each frame.
+//
+// The `EventHandler` trait also contains callbacks for event handling
+// that you can override if you wish, but the defaults are fine.
+impl event::EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
 
-    let piece_text = match piece.rank {
-        Rank::Pawn => 'P',
-        Rank::Knight => 'N',
-        Rank::Bishop => 'B',
-        Rank::Rook => 'R',
-        Rank::Queen => 'Q',
-        Rank::King => 'K',
-        Rank::Empty => '.'
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        let mut canvas =
+            graphics::Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.2, 0.3, 1.0]));
+
+        // Text is drawn from the top-left corner.
+        let offset = self.frames as f32 / 10.0;
+        let dest_point = ggez::glam::Vec2::new(offset, offset);
+        canvas.draw(
+            graphics::Text::new("Hello, world!")
+                .set_font("LiberationMono")
+                .set_scale(48.),
+            dest_point,
+        );
+
+        canvas.finish(ctx)?;
+
+        self.frames += 1;
+        if self.frames.is_multiple_of(100) {
+            println!("FPS: {}", ctx.time.fps());
+        }
+
+        Ok(())
+    }
+}
+
+// Now our main function, which does three things:
+//
+// * First, create a new `ggez::ContextBuilder`
+// object which contains configuration info on things such
+// as screen resolution and window title.
+// * Second, create a `ggez::game::Game` object which will
+// do the work of creating our MainState and running our game.
+// * Then, just call `game.run()` which runs the `Game` mainloop.
+pub fn main() -> GameResult {
+    // We add the CARGO_MANIFEST_DIR/resources to the resource paths
+    // so that ggez will look in our cargo project directory for files.
+    let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+        let mut path = path::PathBuf::from(manifest_dir);
+        path.push("resources");
+        path
+    } else {
+        path::PathBuf::from("./resources")
     };
 
-    if piece.color.eq(&Color::Black) {
-        return piece_text.to_ascii_lowercase();
-    }
-    return piece_text;
+    let cb = ggez::ContextBuilder::new("helloworld", "ggez").add_resource_path(resource_dir);
+    let (mut ctx, event_loop) = cb.build()?;
+
+    let state = MainState::new(&mut ctx)?;
+    event::run(ctx, event_loop, state)
 }
