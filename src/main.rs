732f8ecc658642405a5aa65eb2776e131bee17e0
zlinux <ziltriz@gmail.com>
2026-09-25 01:39:42 +0200

    Implemented win-detection and pawn promotion. Fixes #4 Fixes #5 Fixes #6

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use ggez::{
     Context, GameResult, event, glam::*, graphics::{self, Color, LineCap::Square, Mesh, Rect},
 };
 use std::{env, path};
+use chess::*;
 
 struct MainState {
     // represents a square with a background color and a piece on it
@@ -33,12 +34,12 @@ impl MainState {
 
         // Squares and background colors
         let squares: [Mesh; 64] = std::array::from_fn(|i| {
-            let mut square_color = Color::from_rgb(150, 77, 34);
+            let mut square_color = Color::from_rgb(218, 217, 181);
             let column = i % 8;
             let row = i / 8;
 
             if (column + row)%2 == 0 {
-                square_color = Color::from_rgb(218, 217, 181);
+                square_color = Color::from_rgb(150, 77, 34);
             }
 
             graphics::Mesh::new_rectangle(
@@ -54,7 +55,11 @@ impl MainState {
 
         let clicked_square = None;
 
-        Ok(MainState { squares, game, clicked_square})
+        let turn = chess::Color::White;
+
+        let game_state: String = " ".to_string();
+
+        Ok(MainState { squares, game, clicked_square, turn, game_state})
     }
 }
 
@@ -106,6 +111,42 @@ impl event::EventHandler for MainState {
             )
         }
 
+        if self.game_state == "Checkmate".to_string() || self.game_state == "Stalemate".to_string() {
+            let restart_game = graphics::Mesh::new_rectangle(
+                ctx, 
+                graphics::DrawMode::fill(),
+                Rect::new(100.0, 100.0, 500.0, 150.0), 
+                Color::from_rgba(0, 0, 0, 225)
+            ).expect(" ");
+
+            canvas.draw(&restart_game, Vec2::new(0.0, 0.0));
+
+            let mut restart_text: String = " ".to_string();
+            if self.game_state == "Checkmate".to_string() {
+                if self.turn == chess::Color::White {
+                    restart_text = "Checkmate! Black wins".to_string();
+                }
+                else {
+                    restart_text = "Checkmate! White wins".to_string();
+                }
+            }
+            else if self.game_state == "Stalemate".to_string() {
+                restart_text = "Stalemate!".to_string();
+            }
+
+            canvas.draw(
+                graphics::Text::new(restart_text).set_scale(40.0),
+                Vec2::new(100.0, 100.0)
+            );
+            /*
+            canvas.draw(
+                graphics::Text::new("Press Enter to restart!").set_scale(40.0),
+                Vec2::new(100.0, 200.0)
+            );
+            */
+        }
+    
+
         canvas.finish(ctx)?;
 
         Ok(())
@@ -126,17 +167,36 @@ impl event::EventHandler for MainState {
         if column >= 8 || row >= 8 {
             return Ok(());
         }
-
+        
+        // Finds the square that was clicked on and prints in terminal (mainly for debugging)
         let square_number = (7-row) * 8 + column;
         println!("clicked {}: c{}, r{}", &square_number, column, row);
+        println!("Game_state {}", self.game_state);
 
         if self.clicked_square == None {
             if self.game.check_square(square_number).rank != Rank::Empty {
                 self.clicked_square = Some(square_number)
             }
         } 
-        else {
+        else if self.game_state==" ".to_string() || self.game_state=="Check".to_string() {
+            // Saves the board before the move was made
+            let before_move = self.game;
             self.game.move_piece(self.clicked_square.unwrap(), square_number);
+            if self.game != before_move {
+                // Will only run if the move was made and was valid
+                if self.turn == chess::Color::Black {
+                    self.turn = chess::Color::White
+                } 
+                else {
+                    self.turn = chess::Color::Black
+                }
+                if self.game.check_square(square_number).rank == Rank::Pawn && (square_number / 8 == 7 || square_number / 8 == 0) {
+                    self.game.upgrade_pawn(square_number, Rank::Queen);
+                }
+
+                self.game_state = check_state(&self.game, self.turn);
+            }
+
             self.clicked_square = None;
         }
 
@@ -171,50 +231,6 @@ pub fn main() -> GameResult {
 
 }
 
-use chess::*;
-
-// Init a full board.
-fn fake_main() {
-
-    // init a board object.
-    let mut gameboard = Board::init_board();
-
-    let (start, dest) = (1, 2);
-
-    // fills the created board object.
-    gameboard.fill_board();
-    gameboard.move_piece(start, dest);
-    
-    // Displays board with ascii in CLI
-    cli_display_board(&gameboard);
-}
-
-// Method to display board in CLI
-fn cli_display_board (game: &Board) {
-
-    let mut rank: i32 = 0;
-    for square_number in 0..64 {
-
-        let current_square_piece = game.check_square(square_number);
-
-        // If this is the first square in the rank
-        // Print current rank and a | 
-        if square_number%8 == 0 {
-            rank += 1;
-
-            print!("{} | ", rank)
-        }
-
-        if square_number%8 == 7 {
-            println!("{} ", pice_to_char(current_square_piece) ) 
-        }
-        else {
-            print!("{} ", pice_to_char(current_square_piece) ) 
-        }
-    }
-    println!("------------------");
-    println!("  | a b c d e f g h");
-}
 
 fn pice_to_char (piece: Piece) -> char {
 
@@ -232,4 +248,81 @@ fn pice_to_char (piece: Piece) -> char {
         return piece_text.to_ascii_uppercase();
     }
     return piece_text;
+}
+
+fn check_state(board: &Board, color: chess::Color) -> String {
+
+    // If in check but has legal moves -> Check
+    if in_check(board, color) && has_legal_move(board, color) {
+        "Check".to_string()
+    }
+    // In check no legal moves -> Checkmate
+    else if in_check(board, color) && !has_legal_move(board, color) {
+        "Checkmate".to_string()
+    }
+    // Not in check, no legal moves -> Stalemate
+    else if !in_check(board, color) && !has_legal_move(board, color) {
+        "Stalemate".to_string()
+    }
+    else {
+        " ".to_string()
+    }
+}
+
+fn in_check(board: &Board, color: chess::Color) -> bool {
+
+    // Go through all squares
+    for i in 0..64 {
+
+        // If we find friendly king
+        if board.squares[i].rank == Rank::King && board.squares[i].color == color {
+
+            // Set king square
+            let king_square = i;
+
+            // Go through all squares
+            for i in 0..64 {
+
+                // Check if enemy can see the king
+                let piece = board.squares[i];
+                if piece.rank != Rank::Empty && piece.color != color {
+                    let mut board_copy = *board;
+                    if board_copy.fetch_movelist(i).contains(&king_square) {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+    return false;
+
+}
+
+fn has_legal_move(board: &Board, color: chess::Color) -> bool {
+     // Go through all pieces
+     for start in 0..64 {
+
+        // If we find friendly piece
+        if board.squares[start].color == color {
+            // Save board pre-move
+            let mut board_copy = *board;
+
+            for stop in board_copy.fetch_movelist(start) {
+                let mut board_post_test_move = *board;
+                board_post_test_move.move_piece(start, stop);
+
+                // We found legal move
+                if board_copy != board_post_test_move && !in_check(&board_post_test_move, color){
+                    return true;
+                }
+            }
+
+
+
+        }
+     }
+     return false;
+   
 }
